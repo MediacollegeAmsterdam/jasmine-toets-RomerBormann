8bf53402909296756e2a081592cd0226b0e4da89
Romer Bormann <55235748+RomerBormann@users.noreply.github.com>
2020-10-20 11:09:23 +0200

    Opdracht 2 gemaakt

--- toets/opdracht 2/winkelwagen.js
+++ toets/opdracht 2/winkelwagen.js
@@ -1,4 +1,38 @@
-products = '[{"name" : "t-shirt", "voorraad" : "20"},{"name" : "Sneakers", "voorraad" : "5"}]';
+class Product 
 
-var products = JSON.parse(products);
+{
+    constructor(item, amount, size){ 
 
+        let stock = this; 
+
+        stock.Product = item; 
+
+        stock.voorraad = parseFloat(amount); 
+
+        stock.size = size;
+    } 
+
+    showStock(){
+        console.log(` kleur = ${this.Product} , size = ${this.size}`);
+    }
+         
+        verhoogStockMet(count){ 
+
+        let raise = this; 
+
+            raise.count = parseFloat(count); 
+
+            console.log((this.stock += raise.count)); 
+
+        } 
+
+          
+} 
+
+let Product1 = new Product("T-shirt classic rood", 1, "Small"); 
+let Product2 = new Product("T-shirt classic wit", 2, "Medium"); 
+let Product3 = new Product("T-shirt classic blauw", 3, ";Large"); 
+
+Product2.stock;
+
+Product3.showStock();
